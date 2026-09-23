b6bd71647dc7492d200355fc5397fd5a0da945ac
iasminroque-del <iasminroque@gmail.com>
2026-09-23 09:54:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -269,8 +269,8 @@ letter-spacing:-0.3px;
 
 #barcodeINMETRO{
     width:100px;
-    height:25px;
-    margin-top:7px;
+    height:23px;
+    margin-top:5px;
 
     position:relative;
     top:-1mm;
